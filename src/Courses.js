bc39f00e8cbb8b26ef56d89ebdaef96473697be1
Matthew Hoang <hoangmatthew1999@gmail.com>
2023-01-30 16:55:04 -0800

    added the cards to the courses page

--- src/Courses.js
+++ src/Courses.js
@@ -1,7 +1,29 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 export function Courses() {
     return (
       <div className="Courses">
-        <h1>Courses </h1>
+<Row xs={1} md={2} className="g-4">
+      {Array.from({ length: 4 }).map((_, idx) => (
+        <Col>
+          <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+        </Col>
+      ))}
+    </Row>
       </div>
     );
   }
