b9c4290cbd36e28280a2b4443dc05bbd9f0027c6
Matthew Hoang <hoangmatthew1999@gmail.com>
2023-02-21 11:56:40 -0800

    adding dynamic cards

--- src/Courses.js
+++ src/Courses.js
@@ -5,25 +5,35 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 export function Courses() {
-    return (
-      <div className="Courses">
-<Row xs={1} md={2} className="g-4">
-      {Array.from({ length: 4 }).map((_, idx) => (
+
+  const cardData = [
+    {class: "CS 146",}, 
+    {class: "CMPE 195"}, 
+    {class: "PHYS 50"}, 
+    {class: "ENGR 100W"} 
+  
+  ];
+
+  const renderCard = (cardData, index) => {
+    return(
+      <Row xs={1} md={2} className="g-4">
+      {Array.from({ length: 2 }).map((_, idx) => (
         <Col>
           <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        <Card.Title>{cardData.class}</Card.Title>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
         </Col>
       ))}
     </Row>
+
+  )}
+    return (
+      <div className="Courses">
+        {cardData.map(renderCard)}
       </div>
     );
   }
