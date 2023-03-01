4fa8434952e3499127545b2a445732e9ed39ede8
Matthew Hoang <hoangmatthew1999@gmail.com>
2023-02-28 17:07:28 -0800

    added components to the study materials page

--- src/StudyMaterial.js
+++ src/StudyMaterial.js
@@ -1,7 +1,41 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+
 export function StudyMaterials() {
+  const cardData = [
+    {class: "CS 146",}, 
+    {class: "CMPE 195"}, 
+    {class: "PHYS 50"}, 
+    {class: "ENGR 100W"} 
+  
+  ];
+  const renderCard = (cardData, index) => {
+    return(
+      <Row xs={1} md={2} className="g-4">
+      {Array.from({ length: 2 }).map((_, idx) => (
+        <Col>
+          <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>{cardData.class}</Card.Title>
+        <Button variant="primary" href = {"/study-materials/" + cardData.class}>Go to Class Page </Button>
+      </Card.Body>
+    </Card>
+        </Col>
+      ))}
+    </Row>
+
+  )}
     return (
       <div className="Study Materials">
-        <h1>Study Materials </h1>
+        {cardData.map(renderCard)}
+
+
       </div>
     );
   }
--- src/profile.js
+++ src/profile.js
@@ -12,6 +12,91 @@ export function Profile(){
                     A collection of data which has a data value and a next node
                 </Accordion.Body>
             </Accordion.Item>
+
+            <Accordion.Item eventKey="1">
+                <Accordion.Header>Linked List</Accordion.Header>
+                <Accordion.Body>
+                    A collection of data which has a data value and a next node
+                </Accordion.Body>
+            </Accordion.Item>
+
+            <Accordion.Item eventKey="2">
+                <Accordion.Header>Linked List</Accordion.Header>
+                <Accordion.Body>
+                    A collection of data which has a data value and a next node
+                </Accordion.Body>
+            </Accordion.Item>
+
+            <Accordion.Item eventKey="3">
+                <Accordion.Header>Linked List</Accordion.Header>
+                <Accordion.Body>
+                    A collection of data which has a data value and a next node
+                </Accordion.Body>
+            </Accordion.Item>
+
+            <Accordion.Item eventKey="4">
+                <Accordion.Header>Linked List</Accordion.Header>
+                <Accordion.Body>
+                    A collection of data which has a data value and a next node
+                </Accordion.Body>
+
+            </Accordion.Item>
+            <Accordion.Item eventKey="5">
+                <Accordion.Header>Linked List</Accordion.Header>
+                <Accordion.Body>
+                    A collection of data which has a data value and a next node
+                </Accordion.Body>
+            </Accordion.Item>
+
+            <Accordion.Item eventKey="6">
+                <Accordion.Header>Linked List</Accordion.Header>
+                <Accordion.Body>
+                    A collection of data which has a data value and a next node
+                </Accordion.Body>
+            </Accordion.Item>
+
+            <Accordion.Item eventKey="7">
+                <Accordion.Header>Linked List</Accordion.Header>
+                <Accordion.Body>
+                    A collection of data which has a data value and a next node
+                </Accordion.Body>
+            </Accordion.Item>
+
+            <Accordion.Item eventKey="8">
+                <Accordion.Header>Linked List</Accordion.Header>
+                <Accordion.Body>
+                    A collection of data which has a data value and a next node
+                </Accordion.Body>
+            </Accordion.Item>
+
+            <Accordion.Item eventKey="9">
+                <Accordion.Header>Linked List</Accordion.Header>
+                <Accordion.Body>
+                    A collection of data which has a data value and a next node
+                </Accordion.Body>
+            </Accordion.Item>
+
+            <Accordion.Item eventKey="10">
+                <Accordion.Header>Linked List</Accordion.Header>
+                <Accordion.Body>
+                    A collection of data which has a data value and a next node
+                </Accordion.Body>
+            </Accordion.Item>
+
+            <Accordion.Item eventKey="11">
+                <Accordion.Header>Linked List</Accordion.Header>
+                <Accordion.Body>
+                    A collection of data which has a data value and a next node
+                </Accordion.Body>
+            </Accordion.Item>
+
+            <Accordion.Item eventKey="12">
+                <Accordion.Header>Linked List</Accordion.Header>
+                <Accordion.Body>
+                    A collection of data which has a data value and a next node
+                </Accordion.Body>
+            </Accordion.Item>
+
         </Accordion>
     )
 
